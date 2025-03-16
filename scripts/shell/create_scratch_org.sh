8f0595aabc357fa28728cc7cdee68d278e1b4b8e
wagseye <wagseye@gmail.com>
2025-03-16 12:44:08 -0700

    data-xfer: Adding SFDC load_data script to create_scratch_org script
    Also removed redundant STRIPE_SCRIPTS variable definition

--- scripts/shell/create_scratch_org.sh
+++ scripts/shell/create_scratch_org.sh
@@ -16,11 +16,11 @@ if [ -z "$ORG_ALIAS" ]; then
 fi
 
 sf org create scratch --set-default --definition-file config/project-scratch-def.json --alias $ORG_ALIAS --duration-days 14
-STRIPE_SCRIPTS=$PROJECT_HOME/stripe-service/scripts/shell
 
 # The load_data script is for custom metadata types, and the load creates files to be synced via project deploy.
 # The import_data_flow script directly imports sObject records, so we need to do the deploy first so the types are available for the import
 source $STRIPE_SCRIPTS/load_data.sh
+source $SFDC_SCRIPTS/load_data.sh
 
 sf project deploy start
 
